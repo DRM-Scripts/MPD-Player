d5bc9f24c7ff8f4cb5e629d8b859c427ede4b60e
DRM-Scripts <85300094+DRM-Scripts@users.noreply.github.com>
2023-09-04 10:11:00 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo " /\$$      /\$$ /\$$$$\$$  /\$$$$\$$  /\$$$$\$$  /\$$        /\$$$$\$$  /\$$     /\$$ /\$$$$$$$$ /\$$$$\$$ "
+echo "| \$$\$$    /\$$\$$__\$$  |\$$__\$$  |\$$__\$$  |\$$       /\$$__\$$|  \$$   /\$$/| \$$\$$\$$\$$/\$$__\$$"
+echo "| \$$\$$\  /\$$$$$$\$$  |\$$\$$  \$$  |\$$  \$$  |\$$      |\$$  \$$\$$\  \$$\$$/ | \$$      |\$$\$$\$$"
+echo "| \$$ \$\$$\$$\$$$$\$$\$$  |\$$$$\$$\$$  |\$$  |\$$      |\$$$$\$$\$$\  \$$\$/  | \$$$$   |\$$$$\$$/"
+echo "| \$$  \$ \$\$$\$$\$$\$$/\$$  |\$$____/ | \$$      |\$$__\$$\$$\$$\  \$$\$/   | \$$__/   |\$$__\$$"
+echo "| \$$\  \$ |\$$|\$$      | \$$  |\$$      | \$$      |\$$  |\$$|\$$    |\$$    | \$$      |\$$  \$$\$$"
+echo "| \$$ \/  |\$$|\$$      | \$$$$\$$\$$  | \$$      | \$$$$\$$\$$    |\$$    | \$$$$\$\$$\$$|\$$  |\$$"
+echo "|__/     |__/|__/      |_______/ |__/      |________/|__/  |__/    |__/    |________/|__/  |__/"
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt-get update
+sudo apt-get install -y oracle-java17-installer
+
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xf ffmpeg-release-amd64-static.tar.xz
+cd ffmpeg-6.0-amd64-static/
+sudo mv ffmpeg /usr/bin/
+sudo mv ffprobe /usr/bin/
+cd ..
+
+echo "Menu:"
+echo "1. MPD Player V2"
+echo "2. Lancar MOD"
+echo "3. Zeus MPD Player"
+
+read -p "Enter your choice: " choice
+
+case $choice in
+    1)
+        cd /home
+        wget https://github.com/DRM-Scripts/MPD-Player/raw/master/mpd_player2.deb
+        sudo dpkg -i mpd_player2.deb
+        ;;
+    2)
+        cd /home
+        wget https://github.com/DRM-Scripts/MPD-Player/raw/master/lancar.deb
+        sudo dpkg -i lancar.deb
+        ;;
+    3)
+        cd /home
+        wget https://github.com/DRM-Scripts/MPD-Player/raw/master/zeus.deb
+        sudo dpkg -i zeus.deb
+        ;;
+    *)
+        echo "Invalid choice"
+        ;;
+esac
